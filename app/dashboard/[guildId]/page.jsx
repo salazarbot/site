f05f228322c57e96a2b5f98e839c109a97794b0a
Renato <renato.augusto.almeida.santos@gmail.com>
2026-06-13 23:26:09 -0300

    Revert "Dynamic Metadata"
    This reverts commit 6a1e42bf0eda2ef2b0872d32181e6b6785b0914b.

--- app/dashboard/[guildId]/page.jsx
+++ app/dashboard/[guildId]/page.jsx
@@ -2,20 +2,6 @@ import Header from "@/components/Header";
 import styles from "./page.module.css";
 import Settings from "@/components/Settings";
 
-export async function generateMetadata({ params }) {
-  const { guildId } = await params;
-  
-  const guild = await fetch(`/api/discord/guild?id=${guildId}`).then((res) => res.json());
-
-  return {
-    title: `Dashboard de ${guild.name}`,
-    description: `Configurações e informações do servidor ${guild.name} no Salazar.`,
-    openGraph: {
-      images: [guild.iconUrl],
-    },
-  };
-}
-
 export default async function Dashboard({ params }) {
   const { guildId } = await params;
 
